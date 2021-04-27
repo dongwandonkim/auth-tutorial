fea0d15bb2d324494efee57b9365b740ca43e1a6
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-04-26 21:15:36 -0700

    SERVER: data insertion test

--- server/mongodb.js
+++ server/mongodb.js
@@ -11,6 +11,60 @@ MongoClient.connect(
     if (err) {
       return console.error('Unable to connect to database!', err);
     }
+
+    const db = client.db(databaseName);
+    // db.collection('users').insertOne(
+    //   {
+    //     name: 'Kim',
+    //     age: 27,
+    //   },
+    //   (err, result) => {
+    //     if (err) {
+    //       return console.error('unable to insert');
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection('users').insertMany(
+    //   [
+    //     {
+    //       name: 'cindy',
+    //       age: 40,
+    //     },
+    //     {
+    //       name: 'jeannie',
+    //       age: 38,
+    //     },
+    //   ],
+    //   (err, result) => {
+    //     if (err) return console.log('unable to inster this time');
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    db.collection('tasks').insertMany(
+      [
+        {
+          description: 'Learn mongodb',
+          completed: false,
+        },
+        {
+          description: 'get a job',
+          completed: false,
+        },
+        {
+          description: 'learn MERN stack',
+          completed: false,
+        },
+      ],
+      (err, result) => {
+        if (err) return console.log('unable to insert data this time');
+
+        console.log(result.ops);
+      }
+    );
+
     console.log('Connected correctly');
   }
 );
