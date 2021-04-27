142ed561a03360a277c64a1ca2e4765b336eaf0d
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-04-26 20:53:14 -0700

    SERVER: setup mongodb local env

---
+++ server/mongodb.js
@@ -0,0 +1,16 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    if (err) {
+      return console.error('Unable to connect to database!', err);
+    }
+    console.log('Connected correctly');
+  }
+);
