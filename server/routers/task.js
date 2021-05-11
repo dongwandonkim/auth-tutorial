b2d9a7c80a336fce03eb2d79c571344de5bdef8e
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-05-11 01:07:51 -0700

    implement: search options for Task true/false/all for not providing and option

--- server/routers/task.js
+++ server/routers/task.js
@@ -17,10 +17,20 @@ router.post('/tasks', auth, async (req, res) => {
   }
 });
 
+//GET /tasks?completed=false || /tasks?completed=true
 router.get('/tasks', auth, async (req, res) => {
+  const match = {};
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
   try {
     // const tasks = await Task.find({ owner: req.user._id });
-    await req.user.populate('tasks').execPopulate();
+    await req.user
+      .populate({
+        path: 'tasks',
+        match,
+      })
+      .execPopulate();
 
     if (!req.user.tasks) return res.status(404).send('You dont have any tasks');
     res.send(req.user.tasks);
