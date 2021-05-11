83e67d852fe02face275a8710cec2367c92e8332
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-05-11 01:15:11 -0700

    implement: pagination for tasks

--- server/routers/task.js
+++ server/routers/task.js
@@ -18,6 +18,7 @@ router.post('/tasks', auth, async (req, res) => {
 });
 
 //GET /tasks?completed=false || /tasks?completed=true
+//GET /tasks?limit=10&skip=0
 router.get('/tasks', auth, async (req, res) => {
   const match = {};
   if (req.query.completed) {
@@ -29,6 +30,10 @@ router.get('/tasks', auth, async (req, res) => {
       .populate({
         path: 'tasks',
         match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+        },
       })
       .execPopulate();
 
